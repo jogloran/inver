6aede98313680d8d81e7d7d49442c93de0e398c8
Daniel Tse <daniel@overpunch.com>
2020-06-17 17:14:12 -0700

    slightly optimise stack and zp access directly to ram

--- src/cpu6502.hpp
+++ src/cpu6502.hpp
@@ -17,6 +17,15 @@ public:
 
   inline byte read(word address) { return bus->read(address); }
 
+  inline byte read_direct_byte(word addr) {
+    return bus->ram[addr];
+  }
+
+  inline word read_direct_word(word addr) {
+    word result = read_direct_byte(addr);
+    return result | (read_direct_byte(addr + 1) << 8);
+  }
+
   inline byte read_byte() {
     return read(pc++);
   }
@@ -35,13 +44,13 @@ public:
   void reset();
 
   void push_word(word address) {
-    bus->write(SP_BASE + sp - 1, address & 0xff);
-    bus->write(SP_BASE + sp, address >> 8);
+    bus->ram[SP_BASE + sp - 1] = address & 0xff;
+    bus->ram[SP_BASE + sp] = address >> 8;
     sp -= 2;
   }
 
   void push(byte data) {
-    bus->write(SP_BASE + sp, data);
+    bus->ram[SP_BASE + sp] = data;
     --sp;
   }
 
@@ -51,14 +60,14 @@ public:
 
   // Popping with an empty stack (sp = 0x1ff) must set sp = 0x00 and read from 0x100 instead
   byte pop() {
-    auto result = bus->read(SP_BASE + ((sp + 1) & 0xff));
+    auto result = bus->ram[SP_BASE + ((sp + 1) & 0xff)];
     ++sp;
     return result;
   }
 
   word pop_word() {
-    auto result = (bus->read(SP_BASE + ((sp + 1) & 0xff)) |
-                   (bus->read(SP_BASE + ((sp + 2) & 0xff)) << 8));
+    auto result = (bus->ram[SP_BASE + ((sp + 1) & 0xff)] |
+                   (bus->ram[SP_BASE + ((sp + 2) & 0xff)] << 8));
     sp += 2;
     return result;
   }
@@ -89,6 +98,7 @@ public:
   byte a, x, y;
   byte sp;
   word pc;
+
   union flags_t {
     struct {
       byte C: 1;
@@ -102,11 +112,12 @@ public:
     };
     byte reg;
 
-    template <typename Ar>
+    template<typename Ar>
     void serialize(Ar& ar) {
       ar(reg);
     }
   } p;
+
   long ncycles;
   word cycles_left;
   bool crossed_page;
@@ -126,19 +137,25 @@ public:
     return read(addr_##mode()); \
   }
 
+#define DEFINE_ADDR_MODE_DIRECT(mode, body) \
+  word addr_##mode() body \
+  byte deref_##mode() { \
+    return read_direct_byte(addr_##mode()); \
+  }
+
   DEFINE_ADDR_MODE(abs, {
     return read_word();
   })
 
-  DEFINE_ADDR_MODE(zpg, {
+  DEFINE_ADDR_MODE_DIRECT(zpg, {
     return read_byte();
   })
 
-  DEFINE_ADDR_MODE(zpg_plus_x, {
+  DEFINE_ADDR_MODE_DIRECT(zpg_plus_x, {
     return (read_byte() + x) % 256;
   })
 
-  DEFINE_ADDR_MODE(zpg_plus_y, {
+  DEFINE_ADDR_MODE_DIRECT(zpg_plus_y, {
     return (read_byte() + y) % 256;
   })
 
@@ -156,18 +173,18 @@ public:
 
   DEFINE_ADDR_MODE(x_indirect, {
     byte zp_offset = read_byte();
-    word ptr = read((zp_offset + x) % 256);
-    ptr |= read((zp_offset + x + 1) % 256) << 8;
+    word ptr = read_direct_byte((zp_offset + x) % 256);
+    ptr |= read_direct_byte((zp_offset + x + 1) % 256) << 8;
     return ptr;
   })
 
   DEFINE_ADDR_MODE(indirect_y, {
     byte zp_offset = read_byte();
-    word ptr = read(zp_offset);
+    word ptr = read_direct_byte(zp_offset);
     if (ptr & 0xff) {
       crossed_page = true;
     }
-    ptr |= read((zp_offset + 1) % 256) << 8;
+    ptr |= read_direct_byte((zp_offset + 1) % 256) << 8;
     return ptr + y;
   })
 
@@ -206,7 +223,7 @@ public:
 
   void dump_pc();
 
-  template <Bus::Interrupt rupt>
+  template<Bus::Interrupt rupt>
   void irq() {
     if (rupt == Bus::NMI || !p.I) {
       push_word(pc);
